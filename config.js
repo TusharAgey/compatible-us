7c5f5e8259f1ba6ad91301d04d420b6a1bfeb723
Tushar Agey <agey.tushar3@gmail.com>
2022-11-25 10:11:33 +0530

    generate user fields based on the config at the runtime. rename the output PDF to have username

---
+++ config.js
@@ -0,0 +1,12 @@
+const questionConfig = [
+	{
+		label: 'Full Name',
+		id: 'user-full-name',
+		class: 'col-md-12',
+		required: true,
+		'aria-label': 'Full Name...',
+		readonly: false,
+		type: 'input',
+		dataType: 'text'
+	}
+]
