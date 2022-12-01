45c400690a08ee3c16358720094df14a191a9710
Tushar Agey <agey.tushar3@gmail.com>
2022-12-01 09:41:39 +0530

    introduce config based question generation and pdf generation. Add draft of questions

--- config.js
+++ config.js
@@ -1,12 +1,39 @@
 const questionConfig = [
 	{
-		label: 'Full Name',
-		id: 'user-full-name',
+		label: 'Last Name',
+		id: 'user-last-name',
 		class: 'col-md-12',
 		required: true,
-		'aria-label': 'Full Name...',
+		'aria-label': 'Last Name...',
 		readonly: false,
 		type: 'input',
-		dataType: 'text'
-	}
+		dataType: 'text',
+		pdfField: 'Last Name'
+	},
+	{
+		label: 'Sample single select option',
+		id: 'sample-single-option',
+		class: 'col-md-12',
+		required: true,
+		'aria-label': 'Single option',
+		readonly: false,
+		type: 'select',
+		multiple: false,
+		dataType: 'text',
+		options: ['Option1', 'option2'],
+		pdfField: 'Sample single select option'
+	},
+	{
+		label: 'Sample multi select option',
+		id: 'sample-multi-option',
+		class: 'col-md-12',
+		required: true,
+		'aria-label': 'Multi option',
+		readonly: false,
+		type: 'select',
+		multiple: true,
+		dataType: 'text',
+		options: ['Option1', 'option2','Option1', 'option2'],
+		pdfField: 'Sample multi select option'
+	},
 ]
